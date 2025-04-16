36bb61d4c1720f2da7e73fdf9e333ddc0758a496
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-04-17 00:55:04 +0200

    feat: added function that gets number of rated/want2see items

---
+++ utils/request-details.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+// * About this functions:
+// @param cookie - accepts session cookie as a parameter, param is passed in client side code 
+// @param mediaCategory - vote / want2see - determines if endpoint should get number of rated items or want2see items
+// @param mediaType - film / serial - determines the endpoint that should get number of movies or series in previosly set category
+// * returns {} that contains: 
+// * - number of movies/series that are rated by user 
+// * - 3 params that are already passed into function
+// * - those 3 params, are returned, because they are necessary in request getListOfIDs
+
+// ! This function works only for logged user - if user is not logged info filmweb.pl website, there wont be valid cookie, which means that request will not work properly
+
+async function getRequestDetails(cookie, mediaCategory, mediaType) {
+  const ENDPOINT = `https://www.filmweb.pl/api/v1/logged/${mediaCategory}/${mediaType}/count`
+
+  try {
+    const response = await axios.get(ENDPOINT, {
+      headers: {
+        Cookie: cookie
+      }
+    });
+
+    return {
+      number: response.data,
+      cookie,
+      mediaCategory,
+      mediaType
+    }
+  } catch (error) {
+    console.error('Failed in request details');
+  }
+}
+
+module.exports = getRequestDetails
